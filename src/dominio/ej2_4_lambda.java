07322756cc17ae8737850577934aad1d86920049
Álvaro <ainiguezdislaa@gmail.com>
2023-02-20 17:42:54 +0100

    ej terminado

--- src/dominio/ej2_4_lambda.java
+++ src/dominio/ej2_4_lambda.java
@@ -1,2 +1,9 @@
-package dominio;public class ej2_4_lambda {
+package dominio;
+import java.util.stream.Stream;
+
+public class ej2_4_lambda {
+    public static int sumalista(int[]lista){
+        return Stream.iterate(0, i -> i + 1).limit(lista.length).map(i -> lista[i]).reduce(0, (a, b) -> a + b);
+
+    }
 }
